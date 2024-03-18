c4294cc35e3daf873f9a8e0925aa20f5615338d3
vladyslav.bakanov <vladyslav.bakanov@teamdev.com>
2024-03-18 13:05:10 +0200

    Change the naming of tests

--- embed-code-go/test/indent_test/indent_test.go
+++ embed-code-go/test/indent_test/indent_test.go
@@ -16,7 +16,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package indenttest
+package indent_test
 
 import (
 	"embed-code/embed-code-go/indent"
