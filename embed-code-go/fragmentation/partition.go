db7ea3943623626254ca2f9264ac0c9d0a0ab138
vladyslav.bakanov <vladyslav.bakanov@teamdev.com>
2024-03-19 16:24:03 +0200

    Add partition entity

---
+++ embed-code-go/fragmentation/partition.go
@@ -0,0 +1,14 @@
+package fragmentation
+
+// A code fragment partition.
+//
+// A fragment may consist of a few partitions, collected from different points in the code file. In
+// the resulting doc file, the partitions are joined by the +Configuration::separator+.
+type Partition struct {
+	StartPosition int
+	EndPosition   int
+}
+
+func (partition Partition) Select(lines []string) []string {
+	return lines[partition.StartPosition:partition.EndPosition]
+}
