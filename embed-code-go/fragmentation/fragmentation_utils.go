df709da15be413739ad5fd56e19ca6995368dc4c
vladyslav.bakanov <vladyslav.bakanov@teamdev.com>
2024-03-19 16:23:35 +0200

    Add fragmentation utils functions

---
+++ embed-code-go/fragmentation/fragmentation_utils.go
@@ -0,0 +1,41 @@
+// Copyright 2024, TeamDev. All rights reserved.
+//
+// Redistribution and use in source and/or binary forms, with or without
+// modification, must retain the above copyright notice and the following
+// disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package fragmentation
+
+import (
+	"fmt"
+	"os"
+)
+
+// TODO: make checking for valid encoding
+func shouldFragmentize(file string) bool {
+	info, err := os.Stat(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	isFile := !info.IsDir()
+	isValidEncoding := isValidEncoding(file)
+
+	return isFile && isValidEncoding
+}
+
+func isValidEncoding(file string) bool {
+	return true
+}
