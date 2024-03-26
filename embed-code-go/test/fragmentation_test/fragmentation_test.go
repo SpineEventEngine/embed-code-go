e9d97f82a21e32789e8e7309bd8b142c06879901
vladyslav.bakanov <vladyslav.bakanov@teamdev.com>
2024-03-26 15:07:14 +0200

    Debug: change fragments dir

--- embed-code-go/test/fragmentation_test/fragmentation_test.go
+++ embed-code-go/test/fragmentation_test/fragmentation_test.go
@@ -64,7 +64,7 @@ func (testPreparator FragmentationTestsPreparator) cleanup() {
 
 func buildTestConfig() configuration.Configuration {
 	var config = configuration.NewConfiguration()
-	config.FragmentsDir = "./test/.fragments"
+	config.FragmentsDir = "./build/fragments"
 	config.DocumentationRoot = "./test/resources/docs"
 	config.CodeRoot = "./test/resources/code"
 	return config
