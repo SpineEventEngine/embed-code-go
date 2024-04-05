77aa5c2f16a24bd2a32fd3960b2ed81a6a946f05
vbakanov <vladyslav.bakanov@teamdev.com>
2024-04-05 18:18:04 +0100

    Implement states

--- embed-code-go/embedding/states.go
+++ embed-code-go/embedding/states.go
@@ -1,32 +1,53 @@
 package embedding
 
-import "strings"
+import (
+	"embed-code/embed-code-go/configuration"
+	"embed-code/embed-code-go/embedding_instruction"
+	"fmt"
+	"strings"
+)
 
+const statement = "<embed-code"
+
+type Transition interface {
+	accept(context ParsingContext, config configuration.Configuration)
+	recognize(context ParsingContext) bool
+}
+
+//
 // Finish
+//
+
 type Finish struct{}
 
-func (f Finish) recognize(context ParsingContext) {
-	context.reachedEOF()
+func (f Finish) recognize(context ParsingContext) bool {
+	return context.reachedEOF()
 }
 
-func (f Finish) accept(_, _ interface{}) {
+func (f Finish) accept(context ParsingContext, config configuration.Configuration) {
 }
 
+//
 // Code sample line
+//
+
 type CodeSampleLine struct{}
 
-func (c *CodeSampleLine) recognize(context ParsingContext) bool {
+func (c CodeSampleLine) recognize(context ParsingContext) bool {
 	return !context.reachedEOF() && context.codeFenceStarted
 }
 
-func (c *CodeSampleLine) accept(context ParsingContext, _ interface{}) {
+func (c CodeSampleLine) accept(context ParsingContext, config configuration.Configuration) {
 	context.toNextLine()
 }
 
+//
 // Code fence end
+//
+
 type CodeFenceEnd struct{}
 
-func (c *CodeFenceEnd) recognize(context ParsingContext) bool {
+func (c CodeFenceEnd) recognize(context ParsingContext) bool {
 	// Assuming context is of type `interface{}`.
 	// Implement your logic here.
 	if !context.reachedEOF() {
@@ -36,10 +57,10 @@ func (c *CodeFenceEnd) recognize(context ParsingContext) bool {
 	return false
 }
 
-func (c *CodeFenceEnd) accept(context ParsingContext, _ interface{}) {
+func (c CodeFenceEnd) accept(context ParsingContext, config configuration.Configuration) {
 	// Assuming the two arguments are of type `interface{}`.
 	// Implement your logic here.
-	line := context.currentLine
+	line := context.currentLine()
 	renderSample(context)
 	context.result = append(context.result, line)
 	context.embedding = nil
@@ -49,8 +70,96 @@ func (c *CodeFenceEnd) accept(context ParsingContext, _ interface{}) {
 }
 
 func renderSample(context ParsingContext) {
-	for _, line := range context.embedding.content {
+	for _, line := range context.embedding.Content() {
 		indentation := strings.Repeat(" ", context.codeFenceIndentation)
 		context.result = append(context.result, indentation+line)
+	}
+}
+
+//
+// Code fence end
+//
+
+type CodeFenceStart struct{}
+
+func (c CodeFenceStart) recognize(context ParsingContext) bool {
+	if !context.reachedEOF() {
+		return strings.HasPrefix(strings.TrimSpace(context.currentLine()), "```")
+	}
+	return false
+}
+
+func (c CodeFenceStart) accept(context ParsingContext, config configuration.Configuration) {
+	line := context.currentLine()
+	context.result = append(context.result, line)
+	context.codeFenceStarted = true
+	leadingSpaces := len(line) - len(strings.TrimLeft(line, " "))
+	context.codeFenceIndentation = leadingSpaces
+	context.toNextLine()
+}
+
+//
+//
+//
+
+type BlankLine struct{}
+
+func (b BlankLine) recognize(context ParsingContext) bool {
+	if !context.reachedEOF() && strings.TrimSpace(context.currentLine()) == "" {
+		return !context.codeFenceStarted && context.embedding != nil
+	}
+	return false
+}
+
+func (b BlankLine) accept(context ParsingContext, config configuration.Configuration) {
+	line := context.currentLine()
+	context.result = append(context.result, line)
+	context.toNextLine()
+}
+
+//
+//
+//
+
+type RegularLine struct{}
+
+func (r RegularLine) recognize(context ParsingContext) bool {
+	return true
+}
+
+func (r RegularLine) accept(context ParsingContext, config configuration.Configuration) {
+	line := context.currentLine()
+	context.result = append(context.result, line)
+	context.toNextLine()
+}
+
+//
+//
+//
+
+type EmbedInstructionToken struct{}
+
+func (e EmbedInstructionToken) recognize(context ParsingContext) bool {
+	line := context.currentLine()
+	if context.embedding != nil && !context.reachedEOF() && strings.HasPrefix(strings.TrimSpace(line), statement) {
+		return true
+	}
+	return false
+}
+
+func (e EmbedInstructionToken) accept(context ParsingContext, config configuration.Configuration) {
+	instructionBody := []string{}
+	for !context.reachedEOF() {
+		instructionBody = append(instructionBody, context.currentLine())
+		instruction := embedding_instruction.FromXML(strings.Join(instructionBody, ""), config)
+		context.setEmbedding(instruction)
+		context.result = append(context.result, context.currentLine())
+		context.toNextLine()
+		if context.embedding != nil {
+			break
+		}
+	}
+	if context.embedding == nil {
+		panic(fmt.Sprintf("Failed to parse an embedding instruction. Context: %v", context))
 	}
 }
