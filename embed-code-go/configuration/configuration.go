669638a9e56b6870cd9eafb83f8a60cd4ed500db
vladyslav.bakanov <vladyslav.bakanov@teamdev.com>
2024-04-17 16:28:38 +0300

    Add docs for NewConfigurationWithRoots

--- embed-code-go/configuration/configuration.go
+++ embed-code-go/configuration/configuration.go
@@ -84,6 +84,11 @@ func NewConfiguration() Configuration {
 	}
 }
 
+// Builds the config with given codeRoot and docsRoot.
+//
+// codeRoot — a root directory of the source code to be embedded.
+//
+// docsRoot — a root directory of the documentation files.
 func NewConfigurationWithRoots(codeRoot string, docsRoot string) Configuration {
 	return Configuration{
 		CodeIncludes:      DefaultInclude,
