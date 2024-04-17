d441a24e1eabd7f2d84a58f72ac6bfd6ecc576f9
vladyslav.bakanov <vladyslav.bakanov@teamdev.com>
2024-04-17 15:51:27 +0300

    Add docs for error

--- embed-code-go/embedding/unexpected_diff_error.go
+++ embed-code-go/embedding/unexpected_diff_error.go
@@ -2,6 +2,7 @@ package embedding
 
 import "fmt"
 
+// Describes an error which occurs if not up-to-dated files are found during the checking.
 type UnexpectedDiffError struct {
 	changedFiles []string
 }
