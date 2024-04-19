0978a29d61e6632c4106179fd002a16693d3685e
vbakanov <vladyslav.bakanov@teamdev.com>
2024-04-19 12:24:31 +0100

    Add some tests for CLI

--- embed-code-go/test/cli_test/cli_test.go
+++ embed-code-go/test/cli_test/cli_test.go
@@ -143,3 +143,88 @@ func TestEmbedding(t *testing.T) {
 		cli.CheckCodeSamples(config)
 	}))
 }
+
+func TestRequiredArgsFilled(t *testing.T) {
+	args := cli.Args{
+		DocsRoot: "docs",
+		CodeRoot: "code",
+		Mode:     "embed",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "", validation_message)
+}
+
+func TestModeMissed(t *testing.T) {
+	args := cli.Args{
+		DocsRoot: "docs",
+		CodeRoot: "code",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "Mode must be set.", validation_message)
+}
+
+func TestDocsRootMissed(t *testing.T) {
+	args := cli.Args{
+		CodeRoot: "code",
+		Mode:     "embed",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "If one of code_root and docs_root is set, the another one must be set as well.",
+		validation_message)
+}
+
+func TestConfigAndRootDirsSet(t *testing.T) {
+	args := cli.Args{
+		CodeRoot:       "code",
+		DocsRoot:       "docs",
+		Mode:           "embed",
+		ConfigFilePath: "config.yaml",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "Config path cannot be set when code_root, docs_root or optional params are set.",
+		validation_message)
+}
+
+func TestCorrectConfigFile(t *testing.T) {
+	preparator := newTestsPreparator()
+	preparator.setup()
+	defer preparator.cleanup()
+
+	args := cli.Args{
+		Mode:           "embed",
+		ConfigFilePath: "./test/resources/config_files/correct_config.yml",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "", validation_message)
+
+	config_file_validation_message := cli.ValidateConfigFile(args.ConfigFilePath)
+	assert.Equal(t, "", config_file_validation_message)
+}
+
+func TestConfigFileNotExist(t *testing.T) {
+	args := cli.Args{
+		Mode:           "embed",
+		ConfigFilePath: "/some/path/to/config.yaml",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "", validation_message)
+
+	config_file_validation_message := cli.ValidateConfigFile(args.ConfigFilePath)
+	assert.Equal(t, "The file /some/path/to/config.yaml is not exists.", config_file_validation_message)
+}
+
+func TestConfigFileWithoutDocsRoot(t *testing.T) {
+	preparator := newTestsPreparator()
+	preparator.setup()
+	defer preparator.cleanup()
+
+	args := cli.Args{
+		Mode:           "embed",
+		ConfigFilePath: "./test/resources/config_files/config_without_docs_root.yml",
+	}
+	validation_message := cli.Validate(args)
+	assert.Equal(t, "", validation_message)
+
+	config_file_validation_message := cli.ValidateConfigFile(args.ConfigFilePath)
+	assert.Equal(t, "Config must include both code_root and docs_root fields.", config_file_validation_message)
+}
