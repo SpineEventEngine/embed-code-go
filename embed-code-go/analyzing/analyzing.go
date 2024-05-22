102d45457f4480530e94b12fcc770e05d8d5aa03
vbakanov <vladyslav.bakanov@teamdev.com>
2024-05-22 12:39:51 +0100

    Update analyzing with the new error handlings

--- embed-code-go/analyzing/analyzing.go
+++ embed-code-go/analyzing/analyzing.go
@@ -3,27 +3,11 @@ package analyzing
 import (
 	"embed-code/embed-code-go/configuration"
 	"embed-code/embed-code-go/embedding"
-	"embed-code/embed-code-go/embedding/parsing"
-	"fmt"
-	"os"
-	"path/filepath"
+	"embed-code/embed-code-go/fragmentation"
 	"strings"
 
 	"github.com/bmatcuk/doublestar/v4"
 )
-
-type ProblemFile struct {
-	Context parsing.ParsingContext
-	Error   error
-}
-
-func (p ProblemFile) String() string {
-	relativeMarkdownPath, err := filepath.Rel(p.Context.Embedding.Configuration.DocumentationRoot, p.Context.MarkdownFile)
-	if err != nil {
-		panic(err)
-	}
-	return fmt.Sprintf("%s | %s — %s | %s", relativeMarkdownPath, p.Context.Embedding.CodeFile, p.Context.Embedding.Fragment, p.Error.Error())
-}
 
 func AnalyzeAll(config configuration.Configuration) {
 	var problemFiles []string
@@ -34,26 +18,11 @@ func AnalyzeAll(config configuration.Configuration) {
 		documentationFiles, _ := doublestar.FilepathGlob(globString)
 		for _, documentationFile := range documentationFiles {
 			processor := embedding.NewEmbeddingProcessor(documentationFile, config)
-			context, err := processor.EmbedWithError()
+			err := processor.Embed()
 			if err != nil {
-				problemFiles = append(problemFiles, ProblemFile{Context: context, Error: err}.String())
+				problemFiles = append(problemFiles, err.Error())
 			}
 		}
 	}
-	WriteStringsToFile("problem-files.txt", problemFiles)
-}
-
-func WriteStringsToFile(filepath string, strings []string) {
-	file, err := os.Create(filepath)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	for _, s := range strings {
-		_, err := file.WriteString(s + "\n")
-		if err != nil {
-			panic(err)
-		}
-	}
+	fragmentation.WriteLinesToFile("problem-files.txt", problemFiles)
 }
