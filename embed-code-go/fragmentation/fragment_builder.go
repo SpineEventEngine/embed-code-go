424823a49f6e828c3e5d59e3491e2045de081fc2
Vladyslav Bakanov <vladyslav.bakanov@teamdev.com>
2024-08-07 13:21:35 +0300

    Add file name to the panic message

--- embed-code-go/fragmentation/fragment_builder.go
+++ embed-code-go/fragmentation/fragment_builder.go
@@ -29,8 +29,8 @@ import "fmt"
 // Name — a name of a Fragment.
 type FragmentBuilder struct {
 	CodeFilePath string
-	Partitions []Partition
-	Name string
+	Partitions   []Partition
+	Name         string
 }
 
 //
@@ -48,7 +48,9 @@ func (fragmentBuilder *FragmentBuilder) AddStartPosition(startPosition int) {
 	if len(fragmentBuilder.Partitions) > 0 {
 		lastAddedPartition := fragmentBuilder.Partitions[len(fragmentBuilder.Partitions)-1]
 		if lastAddedPartition.EndPosition == nil {
-			panic("error: the last added partition has no end position")
+			panic(
+				fmt.Sprintf("error: for the file %s, the last added partition has no end position",
+					fragmentBuilder.CodeFilePath))
 		}
 	}
 
