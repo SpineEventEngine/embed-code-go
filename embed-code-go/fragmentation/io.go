fd3585dc4e12df4888069cf978557b97acbfde29
ozmiiova <ozmiiova@cisco.com>
2024-09-05 10:18:23 +0300

    Fix uint32 format

--- embed-code-go/fragmentation/io.go
+++ embed-code-go/fragmentation/io.go
@@ -30,7 +30,7 @@ import (
 // Does nothing if exists.
 func EnsureDirExists(dirPath string) {
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		var readWriteExecPermission uint32 = 777
+		var readWriteExecPermission uint32 = 0777
 		err := os.MkdirAll(dirPath, os.FileMode(readWriteExecPermission))
 		if err != nil {
 			panic(err)
